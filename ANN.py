fe3fa7d733d9f14912e65d3f3e924cc6bf664694
Ankit Nimje <35985469+ankitnimje@users.noreply.github.com>
2020-03-17 16:30:58 -0400

    Add files via upload

---
+++ ANN.py
@@ -0,0 +1,160 @@
+# This code is prepared for Project Assignment 
+# of COMP.5450 (FALL 2018) course, UML,  by Dr. Jerome Braun.
+#
+# References:
+# 1- https://www.kaggle.com/charma69/titanic-data-science-solutions/edit
+
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score, cross_val_predict, \
+									StratifiedKFold, train_test_split
+from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix
+
+from matplotlib import pyplot as plt
+from read_data import *
+from sklearn import metrics
+
+scaler = False
+
+def create_baseline(layers):
+	# create model
+	model = Sequential()
+	model.add(Dense(layers[1], input_dim=layers[0], kernel_initializer='normal', activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.5))
+	if len(layers) > 2:
+		for each in range(len(layers)-2):
+			model.add(Dense(layers[each+1], kernel_initializer='normal', activation='relu'))
+			model.add(BatchNormalization())
+			model.add(Dropout(0.5))
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+def train_and_evaluate_model(model, epoch, X_train, y_train, X_test, y_test):
+	# Fitting our model
+	model.fit(X_train, y_train, batch_size = 10, epochs = epoch)
+	# Predicting the Test set results
+	y_pred = model.predict(X_test)
+	y_pred = (y_pred > 0.5)
+	# Creating the Confusion Matrix
+	cm = confusion_matrix(y_test, y_pred)
+	misclassified = np.where(y_test.reshape(len(y_test), 1) != y_pred.reshape(len(y_pred), 1))
+	misclassified = X_test[misclassified[0],:]
+	return cm, misclassified
+
+def get_misclf_ID(Data, misclassified, scaler):
+	IDs =[]
+	for i in range(len(misclassified)):
+		for j in range(len(misclassified[i])):
+			if not scaler:
+				msc = misclassified[i][j]
+			else:
+				scaler = preprocessing.MinMaxScaler(feature_range = (1,10))
+				msc = scaler.fit_transform(misclassified[i][j])
+			ID = (Data.loc[
+				(Data['ClumpTkns'] == msc[0]) & \
+				(Data['UnofCSize'] == msc[1]) & \
+				(Data['UnofCShape'] == msc[2]) & \
+				(Data['MargAdh'] == msc[3]) & \
+				(Data['SngEpiCSize'] == msc[4]) & \
+				(Data['BareNuc'] == msc[5]) & \
+				(Data['BlandCrmtn'] == msc[6]) & \
+				(Data['NrmlNuc'] == msc[7]) & \
+				(Data['Mitoses'] == msc[8])
+				])
+			IDs.append(int(ID.iloc[0]['ID']))
+	return IDs
+
+#			ID = (Data.loc[
+#				(Data['ClumpTkns'] == msc[0]) & \
+#				(Data['UnofCSize'] == msc[1]) & \
+#				(Data['UnofCShape'] == msc[2]) & \
+#				(Data['MargAdh'] == msc[3]) & \
+#				(Data['SngEpiCSize'] == msc[4]) & \
+#				(Data['BareNuc'] == msc[5]) & \
+#				(Data['BlandCrmtn'] == msc[6]) & \
+#				(Data['NrmlNuc'] == msc[7]) & \
+#				(Data['Mitoses'] == msc[8])
+#				])
+#
+#
+#
+
+
+
+# 1.import data
+dataset = 'WBCD.csv'
+Data = read_data(dataset)
+
+
+# 2.Fill with median
+Data = fillMed(Data)
+
+
+#3.data precessing
+# select features and Normalization
+#x=Data.loc[:,['ClumpTkns', 'UnofCSize', 'UnofCShape', 'MargAdh', 'SngEpiCSize',
+#				'BareNuc', 'BlandCrmtn', 'NrmlNuc', 'Mitoses']].values
+x=Data.loc[:,['ClumpTkns', 'UnofCSize', 'UnofCShape', 'MargAdh', 'SngEpiCSize',
+				'BareNuc', 'BlandCrmtn', 'NrmlNuc', 'Mitoses']].values
+y=Data['Malignant'].values
+
+# get ID NOT working with scaling!!!!
+#scaler = preprocessing.MinMaxScaler(feature_range = (-1,1))
+#x = scaler.fit_transform(x)
+
+
+# 4.train model and performed testing using ANN
+print("\nSelected Algorithm: ANN")
+# evaluate model with standardized dataset
+
+acc_test = []
+misclassified = []
+cms = []
+n_folds = 5
+epoch = 25
+skf = StratifiedKFold(n_splits=n_folds, shuffle=True)
+for i, (train, test) in enumerate(skf.split(x, y)):
+    print ("Running Fold", i+1, "/", n_folds)
+    model = None # Clearing the NN.
+    model = create_baseline([x.shape[1], 12, 6, 1])
+    [cm, misclf] = train_and_evaluate_model(model, epoch, x[train], y[train], x[test], y[test])
+    acc = (cm[0,0]+cm[1,1])/np.sum(cm)
+    cms.append(cm)
+    acc_test.append(acc)
+    misclassified.append(misclf)
+
+print("\nCross-predicted accuracy: {0:.4f}\n".format(np.mean(acc_test)))
+print("\nCross-predicted confusion matrix: \n{}\n".format(sum(cms)))
+IDs = get_misclf_ID(Data, misclassified, scaler)
+IDs.sort()
+print(IDs)
+
+with open('ANN_Total misclassified sample IDs.txt', 'w') as file:
+	file.write("Cross-predicted accuracy: {0:.4f}\n".format(np.mean(acc_test)))
+	file.write("Cross-predicted confusion matrix: \n{}\n".format(sum(cms)))
+	file.write("misclassified sample IDs:\n")
+	for ID in IDs:
+		file.write("%d\n" % ID)
+
+print("\nTotal misclassified number of samples: {}\n".format(len(IDs)))
+#print(len(IDs))
+#print(type(IDs[0]))
+#print(misclassified)
+#print(misclassified[0].shape)
+
+"""
+#submission
+print("Writing submission.csv file...")
+index = [i for i in range(Data.shape[0])]
+df2 = pd.DataFrame({'Predictions': predictions}, index=index)
+submission = pd.concat([Data, df2], axis=1)
+submission.to_csv('wresult.csv', index=False)
+"""
